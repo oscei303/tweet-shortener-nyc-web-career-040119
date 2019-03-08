10c97916b187b0a53bea775ab9439c4e5017a750
OSCAR R <oscr.rss@gmail.com>
2019-03-08 05:08:56 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,32 @@
-# Write your code here.
+require "pry"
+
+def dictionary
+  convert = {
+   "hello" => "hi",
+   "to" => "2",
+   "two" => "2",
+   "too" => "2",
+   "for" => "4",
+   "four" => "4",
+   "be" => "b",
+   "you" => "u",
+   "at" => "@",
+   "and" => "&"
+}
+end
+
+def word_substituter(tweet)
+   tweet.split(" ").map do |word|
+      if dictionary.keys.include?(word.downcase)
+        word = dictionary[word.downcase]
+      else
+        word
+    end
+  end.join(" ")
+end
+
+def bulk_tweet_shortener(tweet)
+  tweet.each do |phrase|
+    puts word_substituter(phrase)
+  end
+end
